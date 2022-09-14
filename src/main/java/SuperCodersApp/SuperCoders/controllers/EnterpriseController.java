7f942aa9094c334428c593f82d6b7b6d95e4f052
erosiano <edwardrossiano@gmail.com>
2022-09-13 20:35:53 -0500

    Esta es una prueba de concepto el cual independiza el manejador de vista, del manejador de datos
    Se crea la vista para Enterprise.
    Se agrega FrontEnterpriseController, el cual se encarga de manejar las peticiones http y solicitar datos al EnterpriseController.  Dejando EnterpriseController como manejador de datos.

--- src/main/java/SuperCodersApp/SuperCoders/controllers/EnterpriseController.java
+++ src/main/java/SuperCodersApp/SuperCoders/controllers/EnterpriseController.java
@@ -17,24 +17,26 @@ public class EnterpriseController {
         this.enterpriseServices = enterpriseServices;
     }
 
-    @GetMapping
+    //@GetMapping
     public List<Enterprise> getAllEnterprise() {
         return this.enterpriseServices.getAllEnterpriseS();
     }
 
-    @GetMapping("/{id}")
+    //@GetMapping("/{id}")
     public Enterprise getEnterprise(@PathVariable("id") long id) {
         return this.enterpriseServices.getEnterpriseS(id);
     }
 
 
-    @PostMapping
-    public Boolean createEnterprise(@RequestBody Enterprise enterprise) {
+    //@PostMapping
+    //public Boolean createEnterprise(@RequestBody Enterprise enterprise) {
+    public Boolean createEnterprise(Enterprise enterprise) {
         return this.enterpriseServices.createEnterpriseS(enterprise);
     }
 
-    @PatchMapping("/{id}")
-    public Boolean updateEnterprise(@PathVariable("id") long id, @RequestBody Enterprise enterprise) {
+    //@PatchMapping("/{id}")
+    //public Boolean updateEnterprise(@PathVariable("id") long id, @RequestBody Enterprise enterprise) {
+    public Boolean updateEnterprise(long id, Enterprise enterprise) {
         Enterprise e1 = this.enterpriseServices.getEnterpriseS(id);
         if (e1 != null) {
             try {
@@ -47,8 +49,9 @@ public class EnterpriseController {
         }
     }
 
-    @DeleteMapping("/{id}")
-    public Boolean deleteEnterprise(@PathVariable("id") long id) {
+    //@DeleteMapping("/{id}")
+    //public Boolean deleteEnterprise(@PathVariable("id") long id) {
+    public Boolean deleteEnterprise(long id) {
         return this.enterpriseServices.deleteEnterpriseS(id);
     }
 }
---
+++ src/main/java/SuperCodersApp/SuperCoders/controllers/FrontEnterpriseController.java
@@ -0,0 +1,75 @@
+package SuperCodersApp.SuperCoders.controllers;
+
+import SuperCodersApp.SuperCoders.entities.Enterprise;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.util.List;
+
+@Controller
+public class FrontEnterpriseController {
+
+    final
+    EnterpriseController enterpriseController;
+
+    public FrontEnterpriseController(EnterpriseController enterpriseController) {
+        this.enterpriseController = enterpriseController;
+    }
+
+    @GetMapping("/enterprise")
+    public String indexEnterprise(Model model, @ModelAttribute("message") String message) {
+        List<Enterprise> listEnterprise = enterpriseController.getAllEnterprise();
+        model.addAttribute("listEnterprise", listEnterprise);
+        model.addAttribute("message", message);
+        return "enterprise/index"; //Llamamos al HTML
+    }
+
+    @GetMapping("/enterprise/new")
+    public String addEnterprise(Model model, @ModelAttribute("message") String message) {
+        Enterprise enterprise = new Enterprise();
+        model.addAttribute("enterprise", enterprise);
+        model.addAttribute("message", message);
+        return "enterprise/add";
+    }
+
+    @PostMapping("/enterprise")
+    public String saveEnterprise(Enterprise enterprise, RedirectAttributes redirectAttributes) {
+        if (enterpriseController.createEnterprise(enterprise)) {
+            redirectAttributes.addFlashAttribute("message", "saveOK");
+            return "redirect:/enterprise";
+        }
+        redirectAttributes.addFlashAttribute("message", "saveError");
+        return "redirect:/enterprise/add";
+    }
+
+    @GetMapping("/enterprise/{id}/edit")
+    public String editEnterprise(Model model, @PathVariable long id, @ModelAttribute("message") String message) {
+        Enterprise enterprise = enterpriseController.getEnterprise(id);
+        //Creamos un atributo para el modelo, que se llame igualmente emp y es el que ira al html para llenar o alimentar campos
+        model.addAttribute("enterprise", enterprise);
+        model.addAttribute("message", message);
+        return "enterprise/edit";
+    }
+
+    @PostMapping("/enterprise/{id}")
+    public String updateEnterprise(@PathVariable long id, @ModelAttribute("enterprise") Enterprise enterprise, RedirectAttributes redirectAttributes) {
+        if (enterpriseController.updateEnterprise(id, enterprise)) {
+            redirectAttributes.addFlashAttribute("message", "updateOK");
+            return "redirect:/enterprise";
+        }
+        redirectAttributes.addFlashAttribute("message", "updateError");
+        return "redirect:/enterprise/edit";
+    }
+
+    @PostMapping("/enterprise/{id}/delete")
+    public String deleteEnterprise(@PathVariable Integer id, RedirectAttributes redirectAttributes) {
+        if (enterpriseController.deleteEnterprise(id)) {
+            redirectAttributes.addFlashAttribute("message", "deleteOK");
+            return "redirect:/enterprise";
+        }
+        redirectAttributes.addFlashAttribute("message", "deleteError");
+        return "redirect:/enterprise";
+    }
+}
