a64ffab52a4dcd5bcaf3497d5e6c08fa3b192de8
erosiano <edwardrossiano@gmail.com>
2022-09-20 22:18:04 -0500

    Boton de inicio de sesion y salida de sesion

--- src/main/java/SuperCodersApp/SuperCoders/controllers/FrontController.java
+++ src/main/java/SuperCodersApp/SuperCoders/controllers/FrontController.java
@@ -1,5 +1,9 @@
 package SuperCodersApp.SuperCoders.controllers;
 
+import SuperCodersApp.SuperCoders.entities.Profile;
+import SuperCodersApp.SuperCoders.services.ProfileService;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,13 +13,22 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 @Controller
 public class FrontController {
 
-    public FrontController() {
-
-    }
+    ProfileController profileController;
 
     @GetMapping("/")
     public String index() {
         return "index"; //Llamamos al HTML
+    }
+
+    @GetMapping("/login")
+    public String login(Model model, @AuthenticationPrincipal OidcUser principal) {
+
+        if(principal != null){
+            System.out.println(principal.getClaims());
+            //Profile profile = this.profileController.createProfile(principal.getClaims());
+            model.addAttribute("user", principal.getClaims());
+        }
+        return  "user/index";
     }
 
 }
