96f7ec5cfdc8f2d0e9c9bfc22e8254132c90566f
erossiano <edwardrossianogmail.com>
2022-09-21 10:41:06 -0500

    Vista de lista, creacion, edicion y eliminacion de empleados.

---
+++ src/main/java/SuperCodersApp/SuperCoders/controllers/FrontEmployeeController.java
@@ -0,0 +1,92 @@
+package SuperCodersApp.SuperCoders.controllers;
+
+import SuperCodersApp.SuperCoders.entities.Employee;
+import SuperCodersApp.SuperCoders.entities.Enterprise;
+import SuperCodersApp.SuperCoders.entities.Profile;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.util.List;
+
+@Controller
+public class FrontEmployeeController {
+
+    final
+    EmployeeController employeeController;
+
+    EnterpriseController enterpriseController;
+    ProfileController profileController;
+
+    public FrontEmployeeController(EmployeeController employeeController, EnterpriseController enterpriseController, ProfileController profileController) {
+        this.employeeController = employeeController;
+        this.enterpriseController = enterpriseController;
+        this.profileController = profileController;
+    }
+
+    @GetMapping("/employee")
+    public String indexEmployee(Model model, @ModelAttribute("message") String message) {
+        List<Employee> listEmployee = employeeController.getAllEmployee();
+        model.addAttribute("listEmployee", listEmployee);
+        model.addAttribute("message", message);
+        return "employee/index"; //Llamamos al HTML
+    }
+
+    @GetMapping("/employee/new")
+    public String addEmployee(Model model, @ModelAttribute("message") String message) {
+        Employee employee = new Employee();
+        List<Enterprise> enterprises = enterpriseController.getAllEnterprise();
+        List<Profile> profiles = profileController.getAllProfile();
+        model.addAttribute("employee", employee);
+        model.addAttribute("enterprises", enterprises);
+        model.addAttribute("profiles", profiles);
+        model.addAttribute("message", message);
+        return "employee/add";
+    }
+
+    @PostMapping("/employee")
+    public String saveEmployee(Employee employee, RedirectAttributes redirectAttributes) {
+        if (employeeController.createEmployee(employee)) {
+            redirectAttributes.addFlashAttribute("message", "saveOK");
+            return "redirect:/employee";
+        }
+        redirectAttributes.addFlashAttribute("message", "saveError");
+        return "redirect:/employee/add";
+    }
+
+    @GetMapping("/employee/{id}/edit")
+    public String editEmployee(Model model, @PathVariable long id, @ModelAttribute("message") String message) {
+        Employee employee = employeeController.getEmployee(id);
+        List<Enterprise> enterprises = enterpriseController.getAllEnterprise();
+        List<Profile> profiles = profileController.getAllProfile();
+        model.addAttribute("employee", employee);
+        model.addAttribute("enterprises", enterprises);
+        model.addAttribute("profiles", profiles);
+        model.addAttribute("message", message);
+        return "employee/edit";
+    }
+
+    @PostMapping("/employee/{id}")
+    public String updateEmployee(@PathVariable long id, @ModelAttribute("employee") Employee employee, RedirectAttributes redirectAttributes) {
+        if (employeeController.updateEmployee(id, employee)) {
+            redirectAttributes.addFlashAttribute("message", "updateOK");
+            return "redirect:/employee";
+        }
+        redirectAttributes.addFlashAttribute("message", "updateError");
+        return "redirect:/employee/edit";
+    }
+
+    @PostMapping("/employee/{id}/delete")
+    public String deleteEmployee(@PathVariable Integer id, RedirectAttributes redirectAttributes) {
+        if (employeeController.deleteEmployee(id)) {
+            redirectAttributes.addFlashAttribute("message", "deleteOK");
+            return "redirect:/employee";
+        }
+        redirectAttributes.addFlashAttribute("message", "deleteError");
+        return "redirect:/employee";
+    }
+}
