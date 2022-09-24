48e38b80302c6677b2ccc27e172f8a7ffd6c140c
erosiano <edwardrossiano@gmail.com>
2022-09-24 12:15:42 -0500

    correccion de heroku 7

--- src/main/java/SuperCodersApp/SuperCoders/controllers/FrontController.java
+++ src/main/java/SuperCodersApp/SuperCoders/controllers/FrontController.java
@@ -20,7 +20,7 @@ public class FrontController {
 
     @GetMapping({"","/", "/index"})
     public String index(Model model) {
-        return  "Index";
+        return  "home";
     }
 
    /* @GetMapping("/")
